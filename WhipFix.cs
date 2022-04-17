3e17f45e23deda8b4c5a43453ad8dfcddcfc730a
59193481 <59193481@qq.com>
2022-04-17 13:21:15 +0800

    fix spears

--- WhipFix.cs
+++ WhipFix.cs
@@ -10,11 +10,13 @@ namespace WhipFix
     {
         public override void Load() {
             base.Load();
-            IL.Terraria.Projectile.AI_165_Whip += Projectile_AI_165_Whip;
+            // 判断代码都是一样的，直接用同一个IL编辑
+            IL.Terraria.Projectile.AI_165_Whip += WhipPatch;
+            IL.Terraria.Projectile.AI_019_Spears += WhipPatch;
         }
 
         // tML大概是itemAnimation为0和1时各判断了一次出手，丢了两个鞭子，刚好使运行到SmartSelectLookup的时候没有itemAnimation为0的空挡期了，切鞭也不能用了
-        private void Projectile_AI_165_Whip(ILContext il) {
+        private void WhipPatch(ILContext il) {
             var c = new ILCursor(il);
             c.GotoNext(MoveType.After, i => i.MatchLdloc(0));
             c.GotoNext(MoveType.After, i => i.MatchLdfld(typeof(Player), nameof(Player.itemAnimation)));
